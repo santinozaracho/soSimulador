ebc66ebd23ed99b82c8bc4c6c2b0fe6d79129a2f
Debra <debranahir@gmail.com>
2018-10-08 20:53:50 -0300

    algoritmo FCFS
    8/10/2018 20.53

--- wepApp/content/app.js
+++ wepApp/content/app.js
@@ -1,18 +1,26 @@
+var sizeMemory = 256;
 var typeMemory = "Fija";
 var fitMemory = "Best Fit";
 var algorithm = "FCFS";
 
 var arrayProcess = [];
 
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
 $(document).ready(function () {
     getData();
     $(".quantumIn").hide();
+
+    $(".optionFitTwo").hide();
 
     console.log(sizeMemory);
     console.log(typeMemory);
     console.log(fitMemory);
     console.log(algorithm);
 
+    $(".sizeInput, .arrivalInput, .firstCpu, .inOut, .lastCpu, .quantumIn, .fixedPart, .inputMemory").keydown(function (e) {
        if ((e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
          (e.keyCode >= 35 && e.keyCode <= 40) ||
          $.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1) {
@@ -23,17 +31,24 @@ $(document).ready(function () {
           e.preventDefault();
       }
 
+      $('.quantumIn').keypress(function() {
+          //falta hacer
+      });
+
     });
 
 
    //control de la obtención del tamaño de la memoria
    $(".optionOne").click(function(){
+      sizeMemory = parseInt($(".optionOne > input").val());
       console.log(sizeMemory)
    });
    $(".optionTwo").click(function(){
+       sizeMemory = parseInt($(".optionTwo > input").val());
        console.log(sizeMemory)
    });
    $(".optionThree").click(function(){
+      sizeMemory = parseInt($(".optionThree > input").val());
       console.log(sizeMemory)
    });
    //--------------------------------
@@ -53,6 +68,8 @@ $(document).ready(function () {
       var valueCurrent = $(".optionTypeTwo > input").val();
       typeMemory = valueCurrent;
       console.log(typeMemory);
+      $("#collapseExample").removeClass("show");
+      //$("#collapseExample").addClass("show");
       $(".optionFitTwo").show();
       $(".optionFitOne").hide();
    });
@@ -106,10 +123,61 @@ $(document).ready(function () {
    //------------------------------------
 
    //seguir
+   $(".startButton").click(function(){
+      $('.progress-bar').clone().addClass('newClass').insertAfter('.progress');
    });
    //--------------
 
 });
+
+
+var cont = 5;
+
+//inputs para crear las particiones
+$(document).on('click', '.btn-add', function(e){
+
+    $('.alertCustom').removeClass('show');
+    $('.alertCustom').addClass('hide');
+
+    e.preventDefault();
+
+      if(cont > 0){
+
+        if($('.inputMemory').val()){
+
+          var controlForm = $('.controls form:first'),
+              currentEntry = $(this).parents('.entry:first'),
+              newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+          newEntry.find('input').val('');
+          controlForm.find('.entry:not(:last) .inputMemory')
+            .addClass('classDisabled')
+            .removeClass('inputMemory')
+            .prop('disabled', true);
+
+          controlForm.find('.entry:not(:last) .btn-add')
+              .removeClass('btn-add').addClass('btn-remove')
+              .removeClass('btn-success').addClass('btn-danger')
+              .html('<span class="glyphicon glyphicon-minus">Quitar</span>');
+
+          cont = cont - 1;
+
+        }else{
+          $('.alertCustom').addClass('show');
+        }
+
+      }else{
+          $('.alertCustom').addClass('show');
+      }
+
+    }).on('click', '.btn-remove', function(e)
+    {
+    $(this).parents('.entry:first').remove();
+
+    e.preventDefault();
+    return false;
+  });
+//-------
 
 var config = {
     apiKey: "AIzaSyBPV-YDy4TwyVtAnKzG8SQ3fwKy4gyAHxQ",
@@ -196,12 +264,9 @@ function getData(){
 
           arrayProcess.push(doc.data());
 
-<<<<<<< HEAD
           console.log(arrayProcess);
 
-          firstInFirstOut(arrayProcess);//borrar despues de probar
-=======
->>>>>>> master
+          firstComeFirstServed(arrayProcess);
 
         });
     });
@@ -212,78 +277,76 @@ function secondStep(){
 }
 
 //algoritmo FCFS
-<<<<<<< HEAD
-//falta agregar arrayProcess como parametro
-function firstInFirstOut(procesosMemoria){
-
+function firstComeFirstServed(procesosMemoria){
    var colaListo = procesosMemoria;
-   var i;
-   var tiempoEspera;
-   var salidaCPU= [];
-   var salidaES= [];
-   var salidaFinal = [];
-   var bandera;
-   var x=0;
-   var controladorBucle = 0;
-   var tiempoEspera;
+   var salidaCPU=[];
+   var salidaES=[];
+   var salidaFinal =[];
+   var salidaTabla=[];
+   var tablaTime=[];
+   var tablaCPU=[];
+   var tablaES=[];
+   var tablaCola= [];
+   var cola=[];
    var enEjecucion = [];
-   var salidaFinal= new Array;
+   var controladorBucle = 0;
+   var i;
+   var tiempo=0;
+   var x=0;
 
-   // for (var i = 0; i < colaListo.length; i++) {
-   //    var firstCpu = parseInt(colaListo[i].ioTime) + parseInt(colaListo[i].cpuTime) + parseInt(colaListo[i].lastCpuTime);
-   //    var controladorBucle =controladorBucle + firstCpu;
-   //    console.log("La duración total del proceso",test);
-   // }
+//obtención de la cantodad de tiempo de procesamiento
+   for (var i = 0; i < colaListo.length; i++) {
+      var firstCpu = parseInt(colaListo[i].ioTime) + parseInt(colaListo[i].cpuTime) + parseInt(colaListo[i].lastCpuTime);
+      var controladorBucle =controladorBucle + firstCpu;
+   }
+//-------------------------------
 
-   for (var x = 0; x < colaListo.length; x++) {
-         enEjecucion = colaListo[x];
-         salidaCPU.push(enEjecucion.name);
-         salidaES.push(enEjecucion.name);
+   for (var i=controladorBucle; i>0; i--) {
+          enEjecucion = colaListo[x];
+	        cola=colaListo.splice(x,1);
+          salidaCPU.push(enEjecucion.name);
+          salidaES.push(enEjecucion.name);
 
           if (enEjecucion.cpuTime!= 0){
             salidaCPU.push( enEjecucion.cpuTime );
             salidaES.push('Ocioso',enEjecucion.cpuTime);
-            colaListo.cpuTime=0;
+            for (i=0; i<colaListo.cpuTime; i++){tablaCPU[i].push(colaListo.name);
+						tablaES[i].push('-');
+						tablaTime[i].push(tiempo);tiempo=tiempo+1;
+						tablaCola[i].push(cola);
+						controladorBucle=controladorBucle-1;}
+	    colaListo.cpuTime=0;
+
            }
          if(enEjecucion.ioTime!=0){
            salidaES.push( enEjecucion.ioTime );
            salidaCPU.push('Ocioso',enEjecucion.ioTime);
+	   for (i=0; i<colaListo.ioTime; i++){tablaCPU[i].push('-');
+						tablaES[i].push(colaListo.name);
+						tablaTime[i].push(tiempo);tiempo=tiempo+1;
+						tablaCola[i].push(cola);
+						controladorBucle=controladorBucle-1;}
            colaListo.ioTime=0;
          }
          if(enEjecucion.lastCpuTime!=0){
            salidaCPU.push( enEjecucion.lastCpuTime );
            salidaES.push('Ocioso',enEjecucion.lastCpuTime);
+	   for (i=0; i<colaListo.lastCpuTime; i++){tablaCPU[i].push(colaListo.name);
+						tablaES[i].push('-');
+						tablaTime[i].push(tiempo);tiempo=tiempo+1;
+						tablaCola[i].push(cola);
+						controladorBucle=controladorBucle-1;}
            colaListo.lastCpuTime=0;
          }
+	x=x+1;
   }
   salidaFinal.push(salidaCPU);
   salidaFinal.push(salidaES);
-
+  salidaTabla.push(tablaTime);
+  salidaTabla.push(tablaCola);
+  salidaTabla.push(tablaCPU);
+  salidaTabla.push(tablaES);
   console.log("Es la salida final", salidaFinal);
-  
+  return(salidaTabla);
+
 }
-=======
-// function FCFS(procesosMemoria){
-//    var colaListo, i, tiempoE, salidaCPU, salidaES, bandera
-//    badera=true
-//    colaListo=procesosMemoria
-//    i=0
-//    while (colaListo!= null && bandera=true){
-//          salidaCPU[]=colaListo[i]
-
-//          if (colaListo[1]!=0 || colaListo[3]!=0){
-//              tiempoE= listaProcesosGeneral[salidaCPU]
-//              if (colaListo[1]!=0){
-//               salidaCPU[1]=arrayProcess
-//               }
-
-
-//           }else{if (colaListo[2]<>0) {
-
-
-//           }else{bandera=false}
-//               }
-//     }
-
-// }
->>>>>>> master
